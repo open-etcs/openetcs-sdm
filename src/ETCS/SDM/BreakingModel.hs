c1b9f261a7f5bfbb86af5e24b1206b220b611670
Markus Barenhoff <mbarenh@alios.org>
2015-09-27 11:43:04 +0200

    finalize BreakingModel

--- src/ETCS/SDM/BreakingModel.hs
+++ src/ETCS/SDM/BreakingModel.hs
@@ -1,10 +1,16 @@
 {-# LANGUAGE Trustworthy #-}
 
 module ETCS.SDM.BreakingModel
-       ( BreakPosition (..), BreakingPercentage, A_Break, T_Break
-       , HasBreakingModelBase(..), a_normal_service
-       , NormalServiceModel, a_break_normal_service
-       , module ETCS.SDM.BreakingModelConverter
+       ( A_Break, T_Break
+       , NormalServiceModels
+       , BreakingModelBase
+       , ConvertingBreakingModelInput
+       , BreakingModel
+       , BreakPosition(..)       
+       , mkBreakingModel
+       , a_break_emergency, t_break_emergency
+       , a_break_service, t_break_service
+       , a_break_normal_service
        ) where
 
 import           Control.Lens hiding ((*~))
@@ -14,21 +20,43 @@ import           ETCS.SDM.Types
 import Numeric.Units.Dimensional.TF.Prelude
 
 
+mkBreakingModel :: (RealFloat f, Floating f) =>
+      NormalServiceModels f -> BreakPosition ->
+      Either (BreakingModelBase f) (ConvertingBreakingModelInput f) ->      
+      Maybe (BreakingModel f)
+mkBreakingModel nsm bpos (Left bm) =
+  mkBreakingModel' nsm bpos bm
+mkBreakingModel nsm bpos (Right i) = 
+  preview breakingModelConverter i >>= mkBreakingModel' nsm bpos
 
+mkBreakingModel'
+  :: HasBreakingModelBase t f =>
+    NormalServiceModels f -> BreakPosition -> t f -> Maybe (BreakingModel f)
+mkBreakingModel' nsm bpos bm = do    
+  bns <- a_break_normal_service' bm nsm bpos
+  return $ BreakingModel {
+    breakingModelBase = toBreakingModelBase bm,
+    a_break_normal_service = bns
+    }
 
-a_break_normal_service ::
+a_break_normal_service' ::
   (HasBreakingModelBase t f, RealFloat f, Floating f) =>
   t f -> NormalServiceModels f -> BreakPosition -> Maybe (A_Break f)
-a_break_normal_service model nsms bpos =
+a_break_normal_service' model nsms bpos =
   let nsmM = nsms ^. case bpos of
         FreightTrainG -> nsms_breakposition_G
         _             -> nsms_breakposition_T
       abs0 = a_break_service model (0 *~ kmh)
-      a_break_normal_service' nsm
+      a_break_normal_service'' nsm
         | (abs0 < nsm ^. nsm_sb01) =
             nsm ^. nsm_model0
         | (abs0 >= nsm ^. nsm_sb01) && (abs0 < nsm ^. nsm_sb12) =
             nsm ^. nsm_model1
         | otherwise =
             nsm ^. nsm_model2
-  in maybe Nothing (Just . a_break_normal_service') nsmM
+  in maybe Nothing (Just . a_break_normal_service'') nsmM
+
+
+
+
+    
--- src/ETCS/SDM/Types.hs
+++ src/ETCS/SDM/Types.hs
@@ -39,7 +39,7 @@ class (Floating f, RealFloat f) => HasBreakingModelBase (t :: * -> *) f where
   a_break_service   :: t f -> A_Break f
   t_break_emergency :: t f -> T_Break f
   t_break_service   :: t f -> T_Break f
-
+  toBreakingModelBase :: t f -> BreakingModelBase f
 
 
 newtype BreakingModelBase f =
@@ -48,8 +48,8 @@ newtype BreakingModelBase f =
 
 data BreakingModel f =
   BreakingModel {
-    breakingModelBase :: BreakingModelBase f,
-    a_normal_service  :: A_Break f
+    breakingModelBase      :: BreakingModelBase f,
+    a_break_normal_service :: A_Break f
     }
 
 data ConvertingBreakingModelInput f =
@@ -102,7 +102,7 @@ instance (Floating f, RealFloat f) => HasBreakingModelBase BreakingModel f where
   a_break_service = a_break_service . breakingModelBase
   t_break_emergency = t_break_emergency . breakingModelBase
   t_break_service = t_break_service . breakingModelBase
-
+  toBreakingModelBase = breakingModelBase
 
 
 instance (Floating f, RealFloat f) => HasBreakingModelBase
@@ -111,7 +111,7 @@ instance (Floating f, RealFloat f) => HasBreakingModelBase
   a_break_service   (BreakingModelBase (_,a,_,_)) = a
   t_break_emergency (BreakingModelBase (_,_,a,_)) = a
   t_break_service   (BreakingModelBase (_,_,_,a)) = a
-
+  toBreakingModelBase = id
 
 instance (Floating f, RealFloat f) =>
          HasBreakingModelBase ConvertedBreakingModel f where
@@ -119,7 +119,12 @@ instance (Floating f, RealFloat f) =>
            a_break_service = a_break_service . _cbmBreakingModel
            t_break_emergency = t_break_emergency . _cbmBreakingModel
            t_break_service = t_break_service  . _cbmBreakingModel
-
+           toBreakingModelBase i =
+             BreakingModelBase ( a_break_emergency i
+                               , a_break_service i
+                               , t_break_emergency i
+                               , t_break_service i
+                               )
 
 instance HasConvertingBreakingModelInput (ConvertedBreakingModel f) f where
   convertingBreakingModelInput = cbmBreakingModelInput
